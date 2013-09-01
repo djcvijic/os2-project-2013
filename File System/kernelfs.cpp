66fb2ae6516aa257df19c8b8360212c729add85b
djcvijic <djordje.cvijic.etf@gmail.com>
2013-09-01 18:06:51 +0200

    Added deallocating memory used by new Cluster(), 1.9.2013.

--- File System/kernelfs.cpp
+++ File System/kernelfs.cpp
@@ -55,14 +55,16 @@ char KernelFS::format(char part){
 		signal(partitionMap[i].idle);
 		return 0;
 	}
-	
+	RootCluster* zeroRootCluster = new RootCluster(0,1);
 	if ((0 == partitionMap[i].partition->getNumOfClusters()) ||
-		(0 == partitionMap[i].partition->writeCluster(0, (char*) new RootCluster(0,1)))){ // Write the zero cluster to partition.
+		(0 == partitionMap[i].partition->writeCluster(0, (char*) zeroRootCluster))){ // Write the zero cluster to partition.
+			delete zeroRootCluster;
 			signal(partitionMap[i].idle);
 			return 0;
 	}
+	delete zeroRootCluster;
 	ClusterNo j = 1;
-	ClusterNo* jPointer = new ClusterNo;
+	ClusterNo* jPointer = new ClusterNo();
 	for (; j < partitionMap[i].partition->getNumOfClusters() - 1; j++){ // Go through all remaining clusters and make a free cluster list.
 		*jPointer = j + 1;
 		partitionMap[i].partition->writeCluster(j, (char*) jPointer);
@@ -81,38 +83,47 @@ char KernelFS::readRootDir(char part, EntryNum n, Directory &d){
 	int iSrc = n, iDest = 0;
 	RootCluster* tempCluster = new RootCluster();
 	if ((partitionMap[i].partition == 0) ||
-		(0 == partitionMap[i].partition->getNumOfClusters() == 0))
-		return 0; // Returns 0 if partition is not mounted or size 0.
+		(0 == partitionMap[i].partition->getNumOfClusters() == 0)){
+			delete tempCluster;
+			return 0; // Returns 0 if partition is not mounted or size 0.
+	}
 	partitionMap[i].partition->readCluster(0, (char*) tempCluster); // Load the zero root cluster.
 	while (iSrc >= ENTRYCNT) { // If n is greater than the number of entries in a root cluster, skip some root clusters until we get to the one where the nth entry is located.
 		iSrc -= ENTRYCNT;
-		if (tempCluster->nextRootCluster == 0)
+		if (tempCluster->nextRootCluster == 0){
+			delete tempCluster;
 			return 0; // If there is no next root cluster, that means n is out of range.
+		}
 		partitionMap[i].partition->readCluster(tempCluster->nextRootCluster, (char*) tempCluster); // Load the next root cluster.
 	}
 
 	wait(partitionMap[i].idle);
 	for (; iSrc < ENTRYCNT; iSrc++, iDest++){ // iSrc now points to the nth root entry, and we start storing it and all subsequent entries in dest...
 		if (tempCluster->rootDirEntries[iSrc].name == 0){ // ... All the while checking whether the current entry is empty in which case we return iDest, the number of read entries...
+			delete tempCluster;
 			signal(partitionMap[i].idle);
 			return iDest;
 		}
 		d[iDest] = tempCluster->rootDirEntries[iSrc];
 	}
 	if (tempCluster->nextRootCluster == 0){ // If there is no next root cluster, we have precisely finished reading entries at the end of the first one, and may return...
+		delete tempCluster;
 		signal(partitionMap[i].idle);
 		return iDest - 1;
 	}
 	partitionMap[i].partition->readCluster(tempCluster->nextRootCluster, (char*) tempCluster); // ... Or else we load the next root cluster.
 	for (iSrc = 0; iDest < ENTRYCNT; iSrc++, iDest++){ // Now we start from the very first entry, and continue filling up dest from where we left off with iDest.
 		if (tempCluster->rootDirEntries[iSrc].name == 0){ // The rest of the loop is same as before.
+			delete tempCluster;
 			signal(partitionMap[i].idle);
 			return iDest;
 		}
 		d[iDest] = tempCluster->rootDirEntries[iSrc];
 	}
+	char noMoreEntries = (tempCluster->rootDirEntries[iSrc].name == 0);
+	delete tempCluster;
 	signal(partitionMap[i].idle);
-	return ENTRYCNT + (tempCluster->rootDirEntries[iSrc].name == 0) ? 0 : 1; // If there are no more entries in the directory, return 64, else return 65.
+	return ENTRYCNT + (noMoreEntries) ? 0 : 1; // If there are no more entries in the directory, return 64, else return 65.
 }
 
 char KernelFS::doesExist(char* fname){
@@ -186,8 +197,10 @@ FileLocation KernelFS::findFile(char* fname){
 	RootCluster* tempCluster = new RootCluster();
 	ClusterNo clusterNo = 0;
 	if ((partitionMap[i].partition == 0) ||
-		(0 == partitionMap[i].partition->getNumOfClusters))
-		return 0; // If partition is not mounted or size 0, return 0.
+		(0 == partitionMap[i].partition->getNumOfClusters)){
+			delete tempCluster;
+			return 0; // If partition is not mounted or size 0, return 0.
+	}
 
 	partitionMap[i].partition->readCluster(clusterNo, (char*) tempCluster); // Load zero root cluster.
 	while (1){	
@@ -195,14 +208,17 @@ FileLocation KernelFS::findFile(char* fname){
 		for (; entry < ENTRYCNT; entry++){ // Go through all the entries. If we come across an empty one, there are no more entries and we can return 0.
 			Entry tempEntry = tempCluster->rootDirEntries[entry];
 			if (tempEntry.name == 0){
+				delete tempCluster;
 				return 0;
 			}
 			if ((0 == strcmp(tempEntry.name, relFName)) && // We check the filename...
 				(0 == strcmp(tempEntry.ext, relFExt))){ // ... As well as the extension...
+					delete tempCluster;
 					return FileLocation { tempEntry, clusterNo, entry }; // ... And if they are both equal, return 1.
 			}
 		}
 		if (tempCluster->nextRootCluster == 0){
+			delete tempCluster;
 			return 0; // If there is no next cluster, return 0...
 		}
 		clusterNo = tempCluster->nextRootCluster;
@@ -233,8 +249,10 @@ FileLocation KernelFS::createFile(char* fname){
 
 	RootCluster* tempCluster = new RootCluster();
 	if ((partitionMap[i].partition == 0) ||
-		(0 == partitionMap[i].partition->getNumOfClusters))
-		return 0; // If partition is not mounted or size 0, return 0.
+		(0 == partitionMap[i].partition->getNumOfClusters)){
+			delete tempCluster;
+			return 0; // If partition is not mounted or size 0, return 0.
+	}
 
 	partitionMap[i].partition->readCluster(clusterNo, (char*) tempCluster); // Load zero root cluster.
 	while (1){
@@ -250,28 +268,40 @@ FileLocation KernelFS::createFile(char* fname){
 		partitionMap[i].partition->readCluster(clusterNo, (char*) tempCluster); // ... And if there is, go to the next one and start over.
 	}
 
-	if (entryNum == ENTRYCNT){
-		RootCluster* zeroRootCluster = new RootCluster();
-		partitionMap[i].partition->readCluster(0, (char*) zeroRootCluster); // Read the zero root cluster.
+	RootCluster* zeroRootCluster, freeCluster;
+	zeroRootCluster = new RootCluster();
+	partitionMap[i].partition->readCluster(0, (char*) zeroRootCluster); // Read the zero root cluster.
 
+	if (entryNum == ENTRYCNT){
 		ClusterNo freeClusterNo = zeroRootCluster->prevRootCluster; // Get the first free cluster number.
-		RootCluster* freeCluster = new RootCluster();
+		freeCluster = new RootCluster();
 		partitionMap[i].partition->readCluster(freeClusterNo, (char*) freeCluster); // Read the first free cluster.
 
-		zeroRootCluster->prevRootCluster = (ClusterNo) freeCluster;
-		partitionMap[i].partition->writeCluster(0, (char*) zeroRootCluster); // Modify the zero root cluster to point to next free cluster, and save it.
+		zeroRootCluster->prevRootCluster = (ClusterNo) (*freeCluster);
 
 		tempCluster->nextRootCluster = freeClusterNo;
 		partitionMap[i].partition->writeCluster(clusterNo, (char*) tempCluster); // Modify the temp cluster to point at the new free one, and save it.
 
 		freeCluster->prevRootCluster = clusterNo; // Modify the free cluster to point at the previous one.
 
-		tempCluster = freeCluster;
+		*tempCluster = *freeCluster;
 		clusterNo = freeClusterNo;
 		entryNum = 0; // And after this, we can call the new free cluster the temp cluster, with the next entry number pointing at the first slot.
 	}
 
+	// Now we'll point the new entry to the first free cluster, and point the zero root cluster to the next free one.
+	newEntry.firstIndexCluster = zeroRootCluster->prevRootCluster;
+	partitionMap[i].partition->readCluster(newEntry.firstIndexCluster, (char*) freeCluster);
+
+	zeroRootCluster->prevRootCluster = (ClusterNo) (*freeCluster);
+	partitionMap[i].partition->writeCluster(0, (char*) zeroRootCluster);
+
 	tempCluster->rootDirEntries[entryNum] = newEntry;
 	partitionMap[i].partition->writeCluster(clusterNo, (char*) tempCluster); // Add the new entry to the cluster, and save it.
+
+	delete tempCluster;
+	delete zeroRootCluster;
+	delete freeCluster;
+
 	return FileLocation { newEntry, clusterNo, entryNum };
 }
