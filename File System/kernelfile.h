112cba7b84806efd0226345dfdcdab94a6a30ebf
djcvijic <djordje.cvijic.etf@gmail.com>
2013-09-02 12:48:15 +0200

    added kernelfile.h, 2.9.2013.

---
+++ File System/kernelfile.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "kernelfs.h"
+#include "fs.h"
+#include "file.h"
+
+class KernelFile {
+public:
+	char write (BytesCnt, char* buffer);
+
+	BytesCnt read (BytesCnt, char* buffer);
+
+	char seek (BytesCnt);
+
+	BytesCnt filePos();
+
+	char eof ();
+
+	BytesCnt getFileSize ();
+
+	char truncate ();
+
+	~KernelFile();
+
+	static File* infoToFile(FileInfo);
+
+private:
+	FileInfo* fileInfo;
+
+	BytesCnt cursor;
+
+	KernelFile ();
+};
